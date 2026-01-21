473e9287c63c81fcbb41d663737d8883082560bd
vasujain275 <vasujain275@gmail.com>
2026-01-21 21:56:59 +0530

    Add sessions SQL queries

---
+++ api/internal/adapters/sqlite/queries/sessions.sql
@@ -0,0 +1,20 @@
+-- name: CreateSession :one
+INSERT INTO revision_sessions (user_id, template_key, planned_duration_min, items_ordered)
+VALUES (?, ?, ?, ?)
+RETURNING *;
+
+-- name: GetSession :one
+SELECT * FROM revision_sessions
+WHERE id = ? AND user_id = ?
+LIMIT 1;
+
+-- name: ListSessionsForUser :many
+SELECT * FROM revision_sessions
+WHERE user_id = ?
+ORDER BY created_at DESC
+LIMIT ? OFFSET ?;
+
+-- name: GetSessionCount :one
+SELECT COUNT(*) as count
+FROM revision_sessions
+WHERE user_id = ?;
